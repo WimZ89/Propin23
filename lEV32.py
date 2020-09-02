a3703fd478b10509fceea9991067fbe69346a31b
WimZu20 <wim.zaagman@allroundtechnology.com>
2020-09-02 14:50:37 +0200

    Initial

---
+++ lEV32.py
@@ -0,0 +1,45 @@
+'''
+Created on Sep 2, 2020
+Proof that it is impossible to make a force without a counteforce
+@author: wimz
+'''
+from math import pi
+from numpy import arange,sin,cos
+import matplotlib.pyplot as plt
+
+
+
+f=1.0 # frequency
+t=0.0 # current time
+tstep=0.05 #time step
+r=1.0   #radius
+
+
+
+def angle(ti):
+    p=[pi/8.0 + pi/8.0 * sin(t*f) for t in ti]
+    return p 
+
+time=arange(0, 2*pi+tstep, tstep )
+
+# for t in  time:
+#     print("%5.2f angle %f"%(t,angle(t)))
+
+# ang=angle(time)
+
+# ang=arange(0, pi/4+tstep, tstep )
+ang=arange(0, pi/2+tstep, tstep )
+
+# print (time)
+# print (ang)
+
+# plt.plot(time,ang)
+# plt.show()
+
+x = [ r*sin(a) for a in ang] 
+y = [ r*cos(a) for a in ang] 
+
+plt.plot(x,y)
+plt.show()
+
+print ("Done")
---
+++ version.py
@@ -0,0 +1,38 @@
+'''
+Created on Aug 21, 2020
+
+@author: wimz
+'''
+import sys
+
+print (sys.version)
+
+from pyexcel_ods import get_data
+
+data = get_data("/media/wimz/TDK_8GB/study/kernkwaliteiten.ods")["kernkwaliteiten"]
+
+# import json
+# print(json.dumps(data, indent=2, separators=(',', ': ')))
+
+header= data.pop(0)
+header = [d.lower() for d in header]
+kekwa,vaku,alle,uitd,vw=header
+# print (header)
+i=1
+cnt=1
+for kwaliteit in data:
+#     kwaliteit = [d.lower() for d in kwaliteit[:4]]
+    if (len(kwaliteit)<5):
+        break
+    for j in range (4):
+        kwaliteit[j]=kwaliteit[j].lower()
+#     print (kwaliteit)
+    voorwerk=kwaliteit[4] > 2
+    if voorwerk :
+        line  = "%d. Voor de %s %s is de %s %s.\n"%(i,kekwa,kwaliteit[0],vaku,kwaliteit[1]) 
+        line += "En de %s is %s, dus de %s is %s\n\n"%(alle,kwaliteit[2],uitd,kwaliteit[3]) 
+    #     line += " is de valkuil " +kwaliteit[1] 
+        cnt+=1
+        print (line)
+    i+=1
+print (i,cnt)

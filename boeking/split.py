b5dbfa53e53fb98cf5aa58082a89973ed97b1621
wimza <zaagwim@gmail.com>
2023-06-17 13:17:59 +0200

    cats

--- boeking/split.py
+++ boeking/split.py
@@ -24,6 +24,21 @@ print(len(Omschrijving))
 
 cat = open("boekingen.txt", "r").readlines()
 print(cat)
-lines = {c.split("\t") for c in cat}
+lines = [c.split("\t") for c in cat]
 
 print(lines)
+new_cat=""
+new_cats = []
+for l in lines:
+    # print(len(l), l[-1])
+    if len(l) == 2:
+        new_cat = l[-1].strip()
+    elif len(l) == 3:
+        add_cat = {"category": new_cat, "description": l[-1].strip()} #, "idx":1}
+        # print(add_cat)
+        new_cats.append(add_cat)
+
+# cats = pd.concat([cats, pd.DataFrame(add_cat)], ignore_index=True)
+#
+cats = pd.DataFrame(new_cats)
+print(cats)

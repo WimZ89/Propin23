e8deaf49524f72929dbdb69c9fc526535abcbac3
wimza <zaagwim@gmail.com>
2023-11-05 13:51:27 +0100

    no 3

--- Uren/UrenAdmin.py
+++ Uren/UrenAdmin.py
@@ -2,10 +2,31 @@
 Created on Dec 28, 2020
 get the hour 
 @author: wimz
+https://docs.google.com/spreadsheets/d/1W-PMcJUuFpVckV7ScBmmIBxrBChOw_Lk1IHXgyk8szU/edit?usp=sharing
+
 '''
 
 from copy import deepcopy
+from datetime import datetime
+
 import pandas as pd
+
+
+def frac_year():
+    date_obj = datetime.now()
+
+    # Calculate the total number of days in the year
+    year = date_obj.year
+    days_in_year = 366 if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)) else 365
+
+    # Calculate the day of the year for the given date
+    day_of_year = (date_obj - datetime(year, 1, 1)).days + 1
+
+    # Calculate the fraction of the year
+    fraction_of_year = day_of_year / days_in_year
+
+    return fraction_of_year
+
 
 f = open("UREN_hom21kum9j6s2s59jtkk6gijn8@group.calendar.google.com.ics", "r")
 lines = f.readlines()
@@ -27,7 +48,7 @@ for i in items:
     for l in i:
         if l.startswith("DTSTART"):
             t = l.split(":")[1].strip()[:8]
-            print(t)
+            # print(t)
         if l.startswith("SUMMARY"):
             s = l.split(":")[1].strip()
             if s.startswith("Werk"):
@@ -46,15 +67,18 @@ for i in items:
 
 df = pd.DataFrame(work2)
 
-for year in ["2020", "2021", "2022", "2023", ]:
-    print()
-    df21 = df[df.date.str[:4] == year]
-    print(df21.to_dict("list"))
-    print(year, df21["hours"].sum())
-    print(df21["hours"].describe())
-    duplicates = df21[df21.duplicated()]
-    if len(duplicates) > 0:
-        print(duplicates)
-    # print(df21["hours"].unique())
-# for w in work:
-#     print(w)
+if __name__ == '__main__':
+    for year in ["2020", "2021", "2022", "2023", ]:  # uren per jaar
+        print()
+        df21 = df[df.date.str[:4] == year]  # filter on year
+        # print(df21.to_dict("list"))
+        print("Totaal uren", year, df21["hours"].sum())
+        print(df21["hours"].describe())
+        duplicates = df21[df21.duplicated()]
+        if len(duplicates) > 0:
+            print(duplicates)
+        # print(df21["hours"].unique())
+    # for w in work:
+    #     print(w)
+
+print("Verwachte uren", year, frac_year()*1818)
--- folders/del_empty.py
+++ folders/del_empty.py
@@ -7,28 +7,28 @@ print(f"Found {len(folders)} folders")
 # [1] [].
 # [2] files in folder.
 
+if __name__ == '__main__':
+    for folder in folders:
+        print(folder)
+        # continue
+        # folder example: ('FOLDER/3', [], ['file'])
+        if not folder[2]:  # folder is empty
+            print("Folder is empty:", folder[0])
+            try:
+                os.rmdir(folder[0])
+                print("Removed empty folder:", folder[0])
+            except OSError:
+                print("OSError: Unable to remove folder", folder[0])
+        else:
+            print("Folder is not empty")
+            ext_to_del = ["txt", "nfo"]
 
-for folder in folders:
-    print(folder)
-    # continue
-    # folder example: ('FOLDER/3', [], ['file'])
-    if not folder[2]:  # folder is empty
-        print("Folder is empty:", folder[0])
-        try:
-            os.rmdir(folder[0])
-            print("Removed empty folder:", folder[0])
-        except OSError:
-            print("OSError: Unable to remove folder", folder[0])
-    else:
-        print("Folder is not empty")
-        ext_to_del = ["txt", "nfo"]
-
-        for filename in folder[2]:
-            file_ext = filename.split(".")[-1]
-            if file_ext in ext_to_del:
-                file_path = os.path.join(folder[0], filename)
-                try:
-                    os.remove(file_path)
-                    print(f"Removed file {filename} in folder {folder[0]}")
-                except OSError:
-                    print(f"OSError: Unable to remove {filename} in folder {folder[0]}")
+            for filename in folder[2]:
+                file_ext = filename.split(".")[-1]
+                if file_ext in ext_to_del:
+                    file_path = os.path.join(folder[0], filename)
+                    try:
+                        os.remove(file_path)
+                        print(f"Removed file {filename} in folder {folder[0]}")
+                    except OSError:
+                        print(f"OSError: Unable to remove {filename} in folder {folder[0]}")
